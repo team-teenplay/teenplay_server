f142edc9469a30942a047b4cf4d8d7999cd53b5e
chorhksdlr <chorhksdlr@gmail.com>
2024-02-29 13:19:01 +0900

    mansgers 수정

--- member/managers.py
+++ member/managers.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class MemberManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().fliter(status=True)
+        return super().get_queryset().fliter(status=1)
     
