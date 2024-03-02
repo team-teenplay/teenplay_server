47b77d4e629b0290c6db77a3109286f1ef2d15c8
kjkj0724 <kjkj0724@gmail.com>
2024-03-02 17:43:02 +0900

    club-intro, club-create done

--- club/urls.py
+++ club/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from club.views import ClubIntroView
+from club.views import ClubIntroView, ClubPrPostsView, ClubCreateView, ClubDetailView
 
 app_name = 'club'
 
 urlpatterns = [
-    path('web/club-intro-web/', ClubIntroView.as_view(), name='club-intro-web'),
+    path('intro/', ClubIntroView.as_view(), name='intro'),
+    path('create/', ClubCreateView.as_view(), name='create'),
+    path('detail/', ClubDetailView.as_view(), name='detail'),
+    path('pr-posts/', ClubPrPostsView.as_view(), name='pr-posts'),
 ]
