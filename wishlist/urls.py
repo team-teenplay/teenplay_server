387675c99af97379aca9b6aa9040cd704f175ed4
jiwon <zznu0521@gmail.com>
2024-03-02 18:10:11 +0900

    urls.py modify

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,7 +5,7 @@ from wishlist.views import WishListView, WishListWriteView, WishListAPI
 app_name = 'wishlist'
 
 urlpatterns = [
-    path('wishlist/', WishListView.as_view(), name='list'),
-    path('wishlist/', WishListWriteView.as_view(), name='write'),
-    path('wishlist/', WishListAPI.as_view(), name='list-api')
+    path('list/', WishListView.as_view(), name='list'),
+    path('list/', WishListWriteView.as_view(), name='write'),
+    path('list/', WishListAPI.as_view(), name='list-api')
 ]
