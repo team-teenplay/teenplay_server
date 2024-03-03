9943c0588371c8c4c6453ecde0d109f9fadcaf15
hyuncoding <hyunstwolte@gmail.com>
2024-03-03 18:55:03 +0900

    commit before pull

--- activity/views.py
+++ activity/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class ActivityCreateView(View):
+    def get(self, request):
+        return render(request, 'activity/web/activity-new-web.html')
+    def post(self, request):
+        pass
