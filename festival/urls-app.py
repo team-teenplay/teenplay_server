f1572f4369893d43db97df63bc973e77d0a7be58
hyuncoding <hyunstwolte@gmail.com>
2024-03-04 16:35:51 +0900

    web/app urls modified, middleware updated

--- festival/urls-app.py
+++ festival/urls-app.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from festival.views import FestivalListAppView, FestivalListAppAPI, FestivalDetailAppView, FestivalDetailAppAPI
 
-app_name = 'festival'
+app_name = 'app-festival'
 
 urlpatterns = [
     # list 페이지로 이동 후 list 페이지 경로 가져오기
@@ -11,5 +11,5 @@ urlpatterns = [
     path('list/<int:page>', FestivalListAppAPI.as_view(), name='festival-list-api'),
     # detail 페이지 이동 후 detail 페이지 경로 가져오기
     path('detail/', FestivalDetailAppView.as_view(), name='festival-detail'),
-    path('detail/<int: post>', FestivalDetailAppAPI.as_view(), name='festival-detail-api'),
+    path('detail/<int:post>', FestivalDetailAppAPI.as_view(), name='festival-detail-api'),
 ]
