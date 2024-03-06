9d664f62e20d23b09d4b2e20d78c7a08d28641d5
jiwon <zznu0521@gmail.com>
2024-03-06 16:45:31 +0900

    module file add

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 
-from wishlist.views import WishListView, WishListWriteView, WishListAPI
+from wishlist.views import WishListView
 
 app_name = 'wishlist'
 
 urlpatterns = [
-    path('list/', WishListView.as_view(), name='list'),
-    path('list/', WishListWriteView.as_view(), name='write'),
-    path('wishlist/<int:page>/', WishListAPI.as_view(), name='list-api'),
-    # path('reply/<int:page>/', WishListReplyAPI.as_view(), name='reply-api')
+    path('list/', WishListView.as_view(), name='list')
 ]
